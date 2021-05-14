4604048d1ad3467a1e4a6a129139c1d5f4936bf5
arthking17 <arthurwilliam.ngassanguesseu@esprit.tn>
2021-05-14 23:23:36 +0100

    first commit

---
+++ src/main/java/tn/kidzoneb/spring/KidZoneBApplication.java
@@ -0,0 +1,40 @@
+package tn.kidzoneb.spring;
+
+import java.util.EnumSet;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
+
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan({"tn.kidzone.spring"})
+public class KidZoneBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidZoneBApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		FacesServlet servlet = new FacesServlet();
+		return new ServletRegistrationBean(servlet, "*.jsf");
+	}
+
+	@Bean
+	public FilterRegistrationBean rewriteFilter() {
+		FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+		rwFilter.setDispatcherTypes(
+				EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR));
+		rwFilter.addUrlPatterns("/*");
+		return rwFilter;
+	}
+}
---
+++ src/test/java/tn/kidzoneb/spring/KidZoneBApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.kidzoneb.spring;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidZoneBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
